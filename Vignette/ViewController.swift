2a63570978fcd3be875d61d60993825d33f48009
csbarlas <62313241+csbarlas@users.noreply.github.com>
2024-05-21 12:00:23 -0400

    Basic search bar and table view

--- Vignette/ViewController.swift
+++ Vignette/ViewController.swift
@@ -8,28 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let fakeData = ["Apple", "Orange", "Pear"]
     
-    private let button: UIButton = {
-        let button = UIButton()
-        button.configuration = .filled()
-        button.setTitle("Hello, World!", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+    // MARK: - UI Components
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Search Movies"
+        searchBar.searchBarStyle = .minimal
+        return searchBar
     }()
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.allowsSelection = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
         
+        self.view.addSubview(searchBar)
+        self.view.addSubview(tableView)
         
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        self.view.addSubview(button)
+        tableView.delegate = self
+        tableView.dataSource = self
         
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         ])
     }
     
+    // MARK: - UI Callbacks
     @objc func didTapButton() {
         let alert = UIAlertController(title: "Hello, User!", message: "You pressed the button!", preferredStyle: .alert)
         let button = UIAlertAction(title: "Dismiss", style: .default, handler: { _ in
@@ -40,5 +60,20 @@ class ViewController: UIViewController {
         DispatchQueue.main.async { [weak self] in
             self?.present(alert, animated: true)
         }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fakeData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = fakeData[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
     }
 }
