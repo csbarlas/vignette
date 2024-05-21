c09e33d08538c3be9f21e06f7867c9fff851c557
csbarlas <62313241+csbarlas@users.noreply.github.com>
2024-05-21 11:22:21 -0400

    Removed Storyboard

--- Vignette/ViewController.swift
+++ Vignette/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.configuration = .filled()
+        button.setTitle("Hello, World!", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
-
-
+    
+    @objc func didTapButton() {
+        let alert = UIAlertController(title: "Hello, User!", message: "You pressed the button!", preferredStyle: .alert)
+        let button = UIAlertAction(title: "Dismiss", style: .default, handler: { _ in
+            print("Button Pressed")
+        })
+        alert.addAction(button)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.present(alert, animated: true)
+        }
+    }
 }
-
